419ef9cdfa447c487b5ed24378ea19cb754ba9b5
ramiroazar <ramiroazar@gmail.com>
2024-07-23 08:55:38 +1000

    Initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,28 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <Link
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/quickstart"
+            rel="noreferrer"
+          >
+            5m Quick Start
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
